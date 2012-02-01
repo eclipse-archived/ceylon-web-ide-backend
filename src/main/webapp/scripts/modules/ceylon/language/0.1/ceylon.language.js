3f74f2fb052f387c839a031b83b102565a202418
Tako Schotanus <tako@ceylon-lang.org>
2012-02-01 22:04:23 +0100

    Updated the compiler and language module to the latest version

--- src/main/webapp/scripts/modules/ceylon/language/0.1/ceylon.language.js
+++ src/main/webapp/scripts/modules/ceylon/language/0.1/ceylon.language.js
@@ -1,4 +1,5 @@
-define(function (require, exports, module) {
+(function(define) {
+    define(function(require, exports, module) {
 
 //the Ceylon language module
 function print(line) { console.log(line.getString().value) }
@@ -366,12 +367,39 @@ $String.prototype.endsWith = function(str) {
     return cmpSubString(this.value, str.value, start);
 }
 $String.prototype.contains = function(sub) {
-    //TODO does this work for unicode, etc?
-    return Boolean$(this.value.indexOf(sub.value) >= 0);
+    var str;
+    if (sub.constructor === $String) {str = sub.value}
+    else if (sub.constructor !== $Character) {return $false}
+    else {str = codepointToString(sub.value)}
+    return Boolean$(this.value.indexOf(str) >= 0);
 }
 $String.prototype.getNormalized = function() {
-    //TODO IMPLEMENT!!!
-    return this;
+    // make use of the fact that all WS characters are single UTF-16 code units
+    var result = "";
+    var len = 0;
+    var first = true;
+    var i1 = 0;
+    while (i1 < this.value.length) {
+        while (this.value.charCodeAt(i1) in $WS) {
+            if (++i1 >= this.value.length) {return String$(result)}
+        }
+        var i2 = i1;
+        var cc = this.value.charCodeAt(i2);
+        do {
+            ++i2;
+            if ((cc&0xfc00) === 0xd800) {++i2}
+            ++len;
+            cc = this.value.charCodeAt(i2);
+        } while (i2<this.value.length && !(cc in $WS));
+        if (!first) {
+            result += " ";
+            ++len;
+        }
+        first = false;
+        result += this.value.substring(i1, i2);
+        i1 = i2+1;
+    }
+    return String$(result, len);
 }
 $String.prototype.firstOccurrence = function(sub) {
     //TODO implement!!!
@@ -382,11 +410,28 @@ $String.prototype.lastOccurrence = function(sub) {
     return null;
 }
 $String.prototype.firstCharacterOccurrence = function(subc) {
-    //TODO implement!!!
+    for (var i=0, count=0; i<this.value.length; count++) {
+        var cp = this.value.charCodeAt(i++);
+        if (((cp&0xfc00) === 0xd800) && i<this.value.length) {
+            cp = (cp<<10) + this.value.charCodeAt(i++) - 0x35fdc00;
+        }
+        if (cp === subc.value) {return Integer(count)}
+    }
+    this.codePoints = count;
     return null;
 }
 $String.prototype.lastCharacterOccurrence = function(subc) {
-    //TODO implement!!!
+    for (var i=this.value.length-1, count=0; i>=0; count++) {
+        var cp = this.value.charCodeAt(i--);
+        if (((cp%0xfc00) === 0xdc00) && i>=0) {
+           cp = (this.value.charCodeAt(i--)<<10) + cp - 0x35fdc00;
+        }
+        if (cp === subc.value) {
+            if (this.codePoints === undefined) {this.codePoints = countCodepoints(this.value)}
+            return Integer(this.codePoints - count - 1);
+        }
+    }
+    this.codePoints = count;
     return null;
 }
 $String.prototype.getCharacters = function() {
@@ -398,8 +443,18 @@ $String.prototype.getKeys = function() {
     return $empty;
 }
 $String.prototype.join = function(strings) {
-    //TODO implement!!!
-    return this;
+    if (strings===undefined || strings.value.length===0) {return String$("", 0)}
+    if (this.codePoints === undefined) {this.codePoints = countCodepoints(this.value)}
+    var str = strings.value[0];
+    var result = str.value;
+    var len = str.codePoints;
+    for (var i=1; i<strings.value.length; ++i) {
+        str = strings.value[i];
+        result += this.value;
+        result += str.value;
+        len += this.codePoints + str.codePoints;
+    }
+    return String$(result, isNaN(len)?undefined:len);
 }
 $String.prototype.split = function(seps, discard) {
     //TODO implement!!!
@@ -1190,6 +1245,13 @@ exports.parseFloat=$parseFloat;
 exports.empty=$empty;
 exports.nullsafe=$nullsafe;
 exports.getInfinity=getInfinity;
-//exports.getNegativeInfinity=getNegativeInfinity;
 exports.className=className;
     });
+}(typeof define==='function' && define.amd ? 
+    define : function (factory) {
+    if (typeof exports!=='undefined') {
+        factory(require, exports, module);
+    } else {
+        throw "no module loader";
+    }
+}));
