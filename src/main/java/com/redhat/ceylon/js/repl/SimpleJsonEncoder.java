cf8c64e6559ff98bf64f8fd90aa2eb4dd7629472
Enrique Zamudio <alterbass@gmail.com>
2012-02-23 19:51:10 -0600

    Implement a very simple JSON encoder instead of hand-encoding the responses

---
+++ src/main/java/com/redhat/ceylon/js/repl/SimpleJsonEncoder.java
@@ -0,0 +1,68 @@
+package com.redhat.ceylon.js.repl;
+
+import java.util.List;
+import java.util.Map;
+
+/** A dead simple JSON encoder. Encoders maps into objects; knows how to encode lists and maps,
+ * and turns anything else into a properly encoded string.
+ * The top-level object must always be a Map.
+ * 
+ * @author Enrique Zamudio
+ */
+public class SimpleJsonEncoder {
+
+    public String encode(Map<String, Object> map) {
+        StringBuilder sb = new StringBuilder();
+        encodeMap(map, sb);
+        return sb.toString();
+    }
+
+    public void encodeString(String s, StringBuilder sb) {
+        sb.append('"');
+        sb.append(s.replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\\\"").replaceAll("'", "\\\\'"));
+        sb.append('"');
+    }
+
+    @SuppressWarnings("unchecked")
+    public void encodeList(List<Object> list, StringBuilder sb) {
+        sb.append('[');
+        boolean first = true;
+        for (Object item : list) {
+            if (!first) {
+                sb.append(',');
+            }
+            if (item instanceof List) {
+                encodeList((List<Object>)item, sb);
+            } else if (item instanceof Map) {
+                encodeMap((Map<String, Object>)item, sb);
+            } else {
+                encodeString(item.toString(), sb);
+            }
+            first=false;
+        }
+        sb.append(']');
+    }
+
+    @SuppressWarnings("unchecked")
+    public void encodeMap(Map<String, Object> map, StringBuilder sb) {
+        sb.append('{');
+        boolean first = true;
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if (!first) {
+                sb.append(',');
+            }
+            encodeString(entry.getKey(), sb);
+            sb.append('=');
+            if (entry.getValue() instanceof List) {
+                encodeList((List<Object>)entry.getValue(), sb);
+            } else if (entry.getValue() instanceof Map) {
+                encodeMap((Map<String, Object>)entry.getValue(), sb);
+            } else {
+                encodeString(entry.getValue().toString(), sb);
+            }
+            first = false;
+        }
+        sb.append('}');
+    }
+
+}
