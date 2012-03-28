134db4afa9f7c427d079bb14c5f8d617ea210354
Enrique Zamudio <alterbass@gmail.com>
2012-03-28 14:26:55 -0600

    Add two implementations of code sharing components (Mongo is the one to use in producion)

---
+++ src/main/java/com/redhat/ceylon/js/codeshare/MongoStorage.java
@@ -0,0 +1,71 @@
+package com.redhat.ceylon.js.codeshare;
+
+import java.net.UnknownHostException;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Default;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+import com.redhat.ceylon.js.repl.CodeKeyGenerator;
+import com.redhat.ceylon.js.repl.CodeStorage;
+
+/** This component stored code snippets using MongoDB.
+ * 
+ * @author Enrique Zamudio
+ */
+@ApplicationScoped @Default
+public class MongoStorage implements CodeStorage {
+
+    private Mongo mongo;
+    private DB db;
+
+    public MongoStorage() throws UnknownHostException, MongoException {
+        String host = System.getenv("OPENSHIFT_NOSQL_DB_HOST");
+        String sport = System.getenv("OPENSHIFT_NOSQL_DB_PORT");
+        String dbname = System.getenv("OPENSHIFT_GEAR_NAME");
+        if (dbname==null) {
+            dbname="ceyloncodez";
+        }
+        String user = System.getenv("OPENSHIFT_NOSQL_DB_USERNAME");
+        String password = System.getenv("OPENSHIFT_NOSQL_DB_PASSWORD");
+        int port = sport == null ? 27017 : Integer.decode(sport);
+        if (host == null) {
+            host = "127.0.0.1";
+        }
+        mongo = new Mongo(host, port);
+        db = mongo.getDB(dbname);
+        if (user != null && password!=null) {
+            if (!db.authenticate(user, password.toCharArray())) {
+                throw new IllegalStateException("MongoDB AUTH failed");
+            }
+        }
+    }
+
+    @Override
+    public String storeCode(String code) {
+        String key = CodeKeyGenerator.generateKey(code);
+        BasicDBObject snip = new BasicDBObject();
+        snip.put("key", key);
+        snip.put("code", code);
+        DBCollection coll = db.getCollection("codez");
+        coll.insert(snip);
+        return key;
+    }
+
+    @Override
+    public String retrieveCode(String key) {
+        DBCollection coll = db.getCollection("codez");
+        DBCursor res = coll.find(new BasicDBObject("key", key));
+        if (res.hasNext()) {
+            return res.next().get("code").toString();
+        }
+        return "//NOT FOUND";
+    }
+
+}
---
+++ src/main/java/com/redhat/ceylon/js/codeshare/SimpleStorage.java
@@ -0,0 +1,31 @@
+package com.redhat.ceylon.js.codeshare;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.enterprise.inject.Alternative;
+
+import com.redhat.ceylon.js.repl.CodeKeyGenerator;
+import com.redhat.ceylon.js.repl.CodeStorage;
+
+/** A simple option to store code snippets: an internal Map.
+ * 
+ * @author Enrique Zamudio
+ */
+@Alternative
+public class SimpleStorage implements CodeStorage {
+
+    private final ConcurrentHashMap<String, String> tehcodez = new ConcurrentHashMap<String, String>();
+
+    @Override
+    public String storeCode(String code) {
+        String key = CodeKeyGenerator.generateKey(code);
+        tehcodez.putIfAbsent(key, code);
+        return key;
+    }
+
+    @Override
+    public String retrieveCode(String key) {
+        return tehcodez.get(key);
+    }
+
+}
