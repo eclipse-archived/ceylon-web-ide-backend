9fbb23b08b4e53cbc931045f9f9455050c12d26c
Tako Schotanus <tako@ceylon-lang.org>
2012-03-28 02:50:54 +0200

    Added browser module to both the ceylon-user-repo (source) and the scripts/modules (JavaScript implementation)

---
+++ src/main/webapp/scripts/modules/browser/1.0.0/browser.dom.js
@@ -0,0 +1,11 @@
+(function(define) {
+    define(function(require, exports, module) {
+	
+    });
+}(typeof define==='function' && define.amd ? define : function (factory) {
+    if (typeof exports!=='undefined') {
+        factory(require, exports, module);
+    } else {
+        throw "no module loader";
+    }
+}));
