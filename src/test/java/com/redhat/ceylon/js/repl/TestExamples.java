87317e180b78492839a6ba5b3496a038eb39a20c
Enrique Zamudio <alterbass@gmail.com>
2013-01-30 23:15:40 -0600

    Test for #34 but for some reason it won't load the browser module

--- src/test/java/com/redhat/ceylon/js/repl/TestExamples.java
+++ src/test/java/com/redhat/ceylon/js/repl/TestExamples.java
@@ -2,7 +2,6 @@ package com.redhat.ceylon.js.repl;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,7 +15,6 @@ import com.redhat.ceylon.cmr.api.RepositoryManager;
 import com.redhat.ceylon.cmr.ceylon.CeylonUtils;
 import com.redhat.ceylon.compiler.Options;
 import com.redhat.ceylon.compiler.js.JsCompiler;
-import com.redhat.ceylon.compiler.js.Stitcher;
 import com.redhat.ceylon.compiler.loader.JsModuleManagerFactory;
 import com.redhat.ceylon.compiler.typechecker.TypeChecker;
 import com.redhat.ceylon.compiler.typechecker.TypeCheckerBuilder;
@@ -37,39 +35,42 @@ public class TestExamples {
         tmpout.deleteOnExit();
         tmpout = tmpout.getParentFile();
         //Copy all files to tmp dir
-        File tmpsrc = File.createTempFile("ceylon", "src");
-        tmpsrc.delete();
-        tmpsrc.mkdir();
-        tmpsrc.deleteOnExit();
+        File tmpRoot = File.createTempFile("ceylon", "src");
+        tmpRoot.delete();
+        tmpRoot.mkdir();
+        tmpRoot.deleteOnExit();
         int count=0;
+        File tmpsrc = new File(tmpRoot, "web_ide_script");
+        tmpsrc.mkdir();
+        //Contents of each file need to be inside a top-level method
         for (File f : srcdir.listFiles()) {
             File dst = new File(tmpsrc, f.getName());
             PrintWriter fout = new PrintWriter(new FileWriter(dst));
-            fout.printf("shared void caca_%d(){", count++);
+            fout.printf("import ceylon.browser{...} import ceylon.browser.dom{...} shared void caca_%d(){", count++);
             BufferedReader fin = new BufferedReader(new FileReader(f));
             String jsline = null;
             while ((jsline = fin.readLine()) != null) {
-                if (!jsline.contains("=require('")) {
-                    fout.println(jsline);
-                }
+                fout.println(jsline);
             }
+            fin.close();
             fout.println("}");
             fout.close();
-            fin.close();
         }
-        args.addAll(Arrays.asList("-src", tmpsrc.getAbsolutePath(),
-                "-optimize",
-                "-rep", "src/main/webapp/scripts/modules",
+        args.addAll(Arrays.asList("-src", tmpRoot.getAbsolutePath(),
+                "-optimize", "-src", "src/main/ceylon",
+                "-rep", "src/main/webapp/WEB-INF/ceylon-repo/system",
                 "-out", tmpout.getAbsolutePath()));
         Options opts = Options.parse(args);
-        TypeCheckerBuilder tcb = new TypeCheckerBuilder().addSrcDirectory(tmpsrc);
         final RepositoryManager repoman = CeylonUtils.repoManager()
                 .systemRepo(opts.getSystemRepo())
                 .userRepos(opts.getRepos())
                 .outRepo(opts.getOutDir())
                 .buildManager();
+        TypeCheckerBuilder tcb = new TypeCheckerBuilder()
+                .addSrcDirectory(new File("./src/main/ceylon"))
+                .addSrcDirectory(tmpRoot);
+                .moduleManagerFactory(new JsModuleManagerFactory());
         tcb.setRepositoryManager(repoman);
-        tcb.moduleManagerFactory(new JsModuleManagerFactory());
         TypeChecker tc = tcb.getTypeChecker();
         tc.process();
         JsCompiler jsc = new JsCompiler(tc, opts);
