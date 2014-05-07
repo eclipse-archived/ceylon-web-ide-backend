d28e74b884b02ee67493c41ca106860b0d7cb599
Enrique Zamudio <alterbass@gmail.com>
2014-05-07 11:37:03 -0500

    tests for documentation extraction

--- src/test/java/com/redhat/ceylon/js/repl/TestAutocomplete.java
+++ src/test/java/com/redhat/ceylon/js/repl/TestAutocomplete.java
@@ -1,12 +1,9 @@
 package com.redhat.ceylon.js.repl;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -52,11 +49,10 @@ public class TestAutocomplete {
         Assert.assertNotNull("No node found at " + assist.getRow() + ":" + assist.getColumn() + " for text " + nodeText, node);
         //Check that the node we found is the one we were actually looking for
         Assert.assertTrue(checkCompletion.startsWith(assist.getTextAtLocation()));
-        JSONArray comps = assist.getCompletions();
+        List<Map<String,Object>> comps = assist.getCompletions();
         String found = null;
-        for (Object o : comps) {
-            JSONObject json = (JSONObject)o;
-            String comp = (String)json.get("insert");
+        for (Map<String,Object> o : comps) {
+            String comp = (String)o.get("insert");
             if (comp.startsWith(checkCompletion)) {
                 found = comp;
             }
---
+++ src/test/java/com/redhat/ceylon/js/repl/TestDoc.java
@@ -0,0 +1,67 @@
+package com.redhat.ceylon.js.repl;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.redhat.ceylon.compiler.typechecker.TypeChecker;
+import com.redhat.ceylon.compiler.typechecker.TypeCheckerBuilder;
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.js.util.CompilerUtils;
+import com.redhat.ceylon.js.util.DocUtils;
+
+@RunWith(Parameterized.class)
+public class TestDoc {
+
+    @Parameters
+    public static Collection<Object[]> sources() {
+        return Arrays.asList(new Object[][]{
+            {"void f(){print(1);}", "ceylon.language::print", 1, 13, true},//false },
+            {"void f(){String{'a','b'};}", "ceylon.language::String", 1, 13, true},//false },
+            {"void f(){print(larger);}", "ceylon.language::larger", 1, 18, true},//false },
+            {"\"local test\"\nshared void test(){}", "web_ide_script::test", 2, 15, true }
+        });
+    }
+
+    private final int row;
+    private final int col;
+    private final String code;
+    private final String dname;
+    private final boolean local;
+
+    public TestDoc(String sourceCode, String declarationName, int row, int column, boolean isLocal) {
+        this.row=row;
+        col=column;
+        code=sourceCode;
+        local=isLocal;
+        dname = declarationName;
+    }
+
+    @Test
+    public void testDocumentation() {
+        TypeChecker tc = new TypeCheckerBuilder().verbose(false).addSrcDirectory(
+                CompilerUtils.createScriptSource(code)).getTypeChecker();
+        tc.process();
+        final Declaration d = DocUtils.findDeclaration(tc, row, col);
+        Assert.assertNotNull("Can't find declaration", d);
+        Assert.assertEquals("Declaration found but not what I expected", dname, d.getQualifiedNameString());
+        final Map<String,Object> docs = DocUtils.getDocs(d);
+        if (local) {
+            String doc = (String)docs.get("doc");
+            Assert.assertNotNull("No local docs found", doc);
+            //Assert.assertTrue("Doc should contain 'local'", doc.contains("local"));
+        } else {
+            String url = (String)docs.get("url");
+            Assert.assertNotNull("No docs found", url);
+            String plainName = dname.substring(dname.indexOf("::") + 2);
+            Assert.assertTrue("Wrong URL", url.contains(plainName));
+        }
+    }
+
+}
