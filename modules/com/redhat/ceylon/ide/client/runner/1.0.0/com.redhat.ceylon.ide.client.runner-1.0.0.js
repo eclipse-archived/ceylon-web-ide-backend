2d7762922c0645845e90f0afa9366e9b5575e926
gavinking <gavin@ceylon-lang.org>
2015-11-04 00:17:21 +0100

    update

--- modules/com/redhat/ceylon/ide/client/runner/1.0.0/com.redhat.ceylon.ide.client.runner-1.0.0.js
+++ modules/com/redhat/ceylon/ide/client/runner/1.0.0/com.redhat.ceylon.ide.client.runner-1.0.0.js
@@ -2,18 +2,18 @@
 
 var _CTM$;function $CCMM$(){if (_CTM$===undefined)_CTM$=require('com/redhat/ceylon/ide/client/runner/1.0.0/com.redhat.ceylon.ide.client.runner-1.0.0-model').$CCMM$;return _CTM$;}
 ex$.$CCMM$=$CCMM$;
-var m$oi6=require('ceylon/language/1.2.0/ceylon.language-1.2.0');
-m$oi6.$addmod$(m$oi6,'ceylon.language/1.2.0');
-m$oi6.$addmod$(ex$,'com.redhat.ceylon.ide.client.runner/1.0.0');
-ex$.$mod$ans$=function(){return[m$oi6.$_native(["js"].$sa$({t:m$oi6.$_String}))];};
-ex$.$pkg$ans$com$redhat$ceylon$ide$client$runner=function(){return[m$oi6.shared()];};
+var m$wlt=require('ceylon/language/1.2.0/ceylon.language-1.2.0');
+m$wlt.$addmod$(m$wlt,'ceylon.language/1.2.0');
+m$wlt.$addmod$(ex$,'com.redhat.ceylon.ide.client.runner/1.0.0');
+ex$.$mod$ans$=function(){return[m$wlt.$_native(["js"].$sa$({t:m$wlt.$_String}))];};
+ex$.$pkg$ans$com$redhat$ceylon$ide$client$runner=function(){return[m$wlt.shared()];};
 
 //MethodDef getOutput at runner.ceylon (1:0-5:0)
 function getOutput(){
 /*BEG dynblock*/
-return (typeof document==='undefined'||document===null?m$oi6.throwexc(m$oi6.Exception("Undefined or null reference: document"),'3:15-3:22','runner.ceylon'):document).getElementById("output");
+return (typeof document==='undefined'||document===null?m$wlt.throwexc(m$wlt.Exception("Undefined or null reference: document"),'3:15-3:22','runner.ceylon'):document).getElementById("output");
 /*END dynblock*/
-};getOutput.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[],d:['com.redhat.ceylon.ide.client.runner','getOutput']};};
+};getOutput.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[],d:['com.redhat.ceylon.ide.client.runner','getOutput']};};
 //MethodDef clearOutput at runner.ceylon (7:0-11:0)
 function clearOutput(){
 /*BEG dynblock*/
@@ -21,69 +21,69 @@ getOutput().innerHTML="";
 /*END dynblock*/
 }
 ex$.clearOutput=clearOutput;
-clearOutput.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[],pa:1,d:['com.redhat.ceylon.ide.client.runner','clearOutput']};};
+clearOutput.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[],pa:1,d:['com.redhat.ceylon.ide.client.runner','clearOutput']};};
 //MethodDef printOutputLine at runner.ceylon (13:0-20:0)
-function printOutputLine($oi7){
+function printOutputLine($wlu){
 /*BEG dynblock*/
-var $oi8=getOutput();
-$oi8.innerHTML=($oi9=($oib=$oi8.innerHTML,$oic=escapeHtml($oi7),(m$oi6.nn$($oib)&&$oib.plus&&$oib.plus($oic))||$oib+$oic),$oia="<br>",(m$oi6.nn$($oi9)&&$oi9.plus&&$oi9.plus($oia))||$oi9+$oia);
+var $wlv=getOutput();
+$wlv.innerHTML=($wlw=($wly=$wlv.innerHTML,$wlz=escapeHtml($wlu),(m$wlt.nn$($wly)&&$wly.plus&&$wly.plus($wlz))||$wly+$wlz),$wlx="<br>",(m$wlt.nn$($wlw)&&$wlw.plus&&$wlw.plus($wlx))||$wlw+$wlx);
 /*END dynblock*/
 }
 ex$.printOutputLine=printOutputLine;
-printOutputLine.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$oi6.$_String}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printOutputLine']};};
+printOutputLine.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$wlt.$_String}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printOutputLine']};};
 //MethodDef printOutput at runner.ceylon (22:0-29:0)
-function printOutput($oid){
+function printOutput($wm0){
 /*BEG dynblock*/
-var $oie=getOutput();
-$oie.innerHTML=($oif=$oie.innerHTML,$oig=escapeHtml($oid),(m$oi6.nn$($oif)&&$oif.plus&&$oif.plus($oig))||$oif+$oig);
+var $wm1=getOutput();
+$wm1.innerHTML=($wm2=$wm1.innerHTML,$wm3=escapeHtml($wm0),(m$wlt.nn$($wm2)&&$wm2.plus&&$wm2.plus($wm3))||$wm2+$wm3);
 /*END dynblock*/
 }
 ex$.printOutput=printOutput;
-printOutput.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$oi6.$_String}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printOutput']};};
+printOutput.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$wlt.$_String}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printOutput']};};
 //MethodDecl escapeHtml at runner.ceylon (31:0-35:33)
-function escapeHtml($oih){return $oih.$_replace("&","&amp;").$_replace("<","&lt;").$_replace(">","&gt;");};
-escapeHtml.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.$_String},ps:[{nm:'html',mt:'prm',$t:{t:m$oi6.$_String}}],d:['com.redhat.ceylon.ide.client.runner','escapeHtml']};};
+function escapeHtml($wm4){return $wm4.$_replace("&","&amp;").$_replace("<","&lt;").$_replace(">","&gt;");};
+escapeHtml.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.$_String},ps:[{nm:'html',mt:'prm',$t:{t:m$wlt.$_String}}],d:['com.redhat.ceylon.ide.client.runner','escapeHtml']};};
 //MethodDecl highlight at runner.ceylon (37:0-40:44)
-function highlight($oii){return m$oi6.sum(m$oi6.interleave([escapeHtml($oii).$_split(m$oi6.$JsCallable(($oij=m$oi6.Character(39,true),m$oi6.JsCallable($oij,m$oi6.nn$($oij)?$oij.equals:null)),[{nm:'that',mt:'prm',$t:{t:m$oi6.$_Object}}],{Arguments$Callable:m$oi6.mtt$([{t:m$oi6.$_Object}]),Return$Callable:{t:m$oi6.$_Boolean}})),["<code>","</code>"].rt$({t:m$oi6.$_String},1).cycled].$sa$({t:m$oi6.Iterable,a:{Element$Iterable:{t:m$oi6.$_String},Absent$Iterable:{t:m$oi6.Nothing}}}),{Absent$interleave:{t:m$oi6.Nothing},Element$interleave:{t:m$oi6.$_String}}),{Value$sum:{t:m$oi6.$_String}});};
-highlight.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.$_String},ps:[{nm:'txt',mt:'prm',$t:{t:m$oi6.$_String}}],d:['com.redhat.ceylon.ide.client.runner','highlight']};};
-var $oij;
+function highlight($wm5){return m$wlt.sum(m$wlt.interleave([escapeHtml($wm5).$_split(m$wlt.$JsCallable(($wm6=m$wlt.Character(39,true),m$wlt.JsCallable($wm6,m$wlt.nn$($wm6)?$wm6.equals:null)),[{nm:'that',mt:'prm',$t:{t:m$wlt.$_Object}}],{Arguments$Callable:m$wlt.mtt$([{t:m$wlt.$_Object}]),Return$Callable:{t:m$wlt.$_Boolean}})),["<code>","</code>"].rt$({t:m$wlt.$_String},1).cycled].$sa$({t:m$wlt.Iterable,a:{Element$Iterable:{t:m$wlt.$_String},Absent$Iterable:{t:m$wlt.Nothing}}}),{Absent$interleave:{t:m$wlt.Nothing},Element$interleave:{t:m$wlt.$_String}}),{Value$sum:{t:m$wlt.$_String}});};
+highlight.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.$_String},ps:[{nm:'txt',mt:'prm',$t:{t:m$wlt.$_String}}],d:['com.redhat.ceylon.ide.client.runner','highlight']};};
+var $wm6;
 //MethodDef createMessagesTable at runner.ceylon (42:0-49:0)
 function createMessagesTable(){
 /*BEG dynblock*/
-if(!(typeof document==='undefined'||document===null?m$oi6.throwexc(m$oi6.Exception("Undefined or null reference: document"),'44:13-44:20','runner.ceylon'):document).getElementById("messages")){
+if(!(typeof document==='undefined'||document===null?m$wlt.throwexc(m$wlt.Exception("Undefined or null reference: document"),'44:13-44:20','runner.ceylon'):document).getElementById("messages")){
 getOutput().innerHTML="<table><tbody id=\'messages\'/></table>";
 }
 /*END dynblock*/
-};createMessagesTable.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[],d:['com.redhat.ceylon.ide.client.runner','createMessagesTable']};};
+};createMessagesTable.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[],d:['com.redhat.ceylon.ide.client.runner','createMessagesTable']};};
 //MethodDecl printSystem at runner.ceylon (51:0-52:50)
-function printSystem($oik,$oil){return printFormatted($oik,$oil,"jsc_msg","");};
-printSystem.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$oi6.$_String}},{nm:'loc',mt:'prm',$t:{t:'u',l:[{t:m$oi6.Null},{t:m$oi6.$_String}]}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printSystem']};};
+function printSystem($wm7,$wm8){return printFormatted($wm7,$wm8,"jsc_msg","");};
+printSystem.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$wlt.$_String}},{nm:'loc',mt:'prm',$t:{t:'u',l:[{t:m$wlt.Null},{t:m$wlt.$_String}]}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printSystem']};};
 ex$.printSystem=printSystem;
 //MethodDecl printWarning at runner.ceylon (54:0-56:46)
-function printWarning($oim,$oin){return printFormatted($oim,$oin,"jsc_warn","images/warning.gif");};
-printWarning.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$oi6.$_String}},{nm:'loc',mt:'prm',$t:{t:'u',l:[{t:m$oi6.Null},{t:m$oi6.$_String}]}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printWarning']};};
+function printWarning($wm9,$wma){return printFormatted($wm9,$wma,"jsc_warn","images/warning.gif");};
+printWarning.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$wlt.$_String}},{nm:'loc',mt:'prm',$t:{t:'u',l:[{t:m$wlt.Null},{t:m$wlt.$_String}]}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printWarning']};};
 ex$.printWarning=printWarning;
 //MethodDecl printError at runner.ceylon (58:0-60:45)
-function printError($oio,$oip){return printFormatted($oio,$oip,"jsc_error","images/error.gif");};
-printError.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$oi6.$_String}},{nm:'loc',mt:'prm',$t:{t:'u',l:[{t:m$oi6.Null},{t:m$oi6.$_String}]}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printError']};};
+function printError($wmb,$wmc){return printFormatted($wmb,$wmc,"jsc_error","images/error.gif");};
+printError.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$wlt.$_String}},{nm:'loc',mt:'prm',$t:{t:'u',l:[{t:m$wlt.Null},{t:m$wlt.$_String}]}}],pa:1,d:['com.redhat.ceylon.ide.client.runner','printError']};};
 ex$.printError=printError;
 //MethodDef printFormatted at runner.ceylon (62:0-74:0)
-function printFormatted($oiq,$oir,$ois,$oit){
+function printFormatted($wmd,$wme,$wmf,$wmg){
 createMessagesTable();
 /*BEG dynblock*/
-var $oiu=getOutput();
-$oiu.innerHTML=($oiv=$oiu.innerHTML,$oiw="<tr>\n<td><img src=\'".plus($oit).plus("\' alt=\'warning\'/>&nbsp;</td>\n<td>").plus(($oix=$oir,m$oi6.nn$($oix)?$oix:"")).plus("</td><td> &mdash; </td>\n<td class=\'").plus($ois).plus("\'>").plus(highlight($oiq)).plus("</td>\n</tr>"),(m$oi6.nn$($oiv)&&$oiv.plus&&$oiv.plus($oiw))||$oiv+$oiw);
+var $wmh=getOutput();
+$wmh.innerHTML=($wmi=$wmh.innerHTML,$wmj="<tr>\n<td><img src=\'".plus($wmg).plus("\' alt=\'warning\'/>&nbsp;</td>\n<td>").plus(($wmk=$wme,m$wlt.nn$($wmk)?$wmk:"")).plus("</td><td> &mdash; </td>\n<td class=\'").plus($wmf).plus("\'>").plus(highlight($wmd)).plus("</td>\n</tr>"),(m$wlt.nn$($wmi)&&$wmi.plus&&$wmi.plus($wmj))||$wmi+$wmj);
 /*END dynblock*/
-var $oix;
-};printFormatted.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$oi6.$_String}},{nm:'loc',mt:'prm',$t:{t:'u',l:[{t:m$oi6.Null},{t:m$oi6.$_String}]}},{nm:'cssClass',mt:'prm',$t:{t:m$oi6.$_String}},{nm:'image',mt:'prm',$t:{t:m$oi6.$_String}}],d:['com.redhat.ceylon.ide.client.runner','printFormatted']};};
+var $wmk;
+};printFormatted.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[{nm:'txt',mt:'prm',$t:{t:m$wlt.$_String}},{nm:'loc',mt:'prm',$t:{t:'u',l:[{t:m$wlt.Null},{t:m$wlt.$_String}]}},{nm:'cssClass',mt:'prm',$t:{t:m$wlt.$_String}},{nm:'image',mt:'prm',$t:{t:m$wlt.$_String}}],d:['com.redhat.ceylon.ide.client.runner','printFormatted']};};
 //MethodDef scrollOutput at runner.ceylon (76:0-80:0)
 function scrollOutput(){
 /*BEG dynblock*/
-(typeof window==='undefined'||window===null?m$oi6.throwexc(m$oi6.Exception("Undefined or null reference: window"),'78:8-78:13','runner.ceylon'):window).scrollTo(0,9999999);
+(typeof window==='undefined'||window===null?m$wlt.throwexc(m$wlt.Exception("Undefined or null reference: window"),'78:8-78:13','runner.ceylon'):window).scrollTo(0,9999999);
 /*END dynblock*/
 }
 ex$.scrollOutput=scrollOutput;
-scrollOutput.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$oi6.Anything},ps:[],pa:1,d:['com.redhat.ceylon.ide.client.runner','scrollOutput']};};
+scrollOutput.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$wlt.Anything},ps:[],pa:1,d:['com.redhat.ceylon.ide.client.runner','scrollOutput']};};
 ex$.$pkgunsh$com$redhat$ceylon$ide$client$runner={'getOutput':getOutput,'escapeHtml':escapeHtml,'highlight':highlight,'createMessagesTable':createMessagesTable,'printFormatted':printFormatted};
 });
 }(typeof define==='function' && define.amd ? define : function (factory) {
